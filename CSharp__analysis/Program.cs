660465bc825caa1b45eecf302a51d969f7224fab
Ariel Lapinin <ariel.lapinin@beyondverbal.com>
2017-05-24 17:02:39 +0300

    Update Program.cs

--- CSharp__analysis/Program.cs
+++ CSharp__analysis/Program.cs
@@ -14,7 +14,7 @@ namespace CSharp__analysis
     {
         public string tokenUrl = "https://token.beyondverbal.com/token";
         public string apiKey = "Enter API Key";
-        public string startUrl = "https://testapiv3.beyondverbal.com/v3/recording/";
+        public string startUrl = "https://apiv3.beyondverbal.com/v3/recording/";
         public string postFilePath = @"C:\YouFile.wav";
     }
     class Program
